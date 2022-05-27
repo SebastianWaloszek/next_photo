53f3f6b23fd0e8fb4798b6b8d9e0601183e5529f
Sebastian Waloszek <sebastian_x95x@tlen.pl>
2022-05-28 00:05:47 +0200

    feat: adjust macOS app

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:next_photo/src/common/presentation/theme/app_icons.dart';
